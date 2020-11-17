87f7022029ad25b811fc03181b5baf00815f88ec
raluca-san <ralucam.sandu@gmail.com>
2020-11-17 11:58:23 +0100

    Updating site source Tue, Nov 17, 2020 11:58:23 AM

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin main
+git push origin HEAD:main
 
 printf "Deploying updates to GitHub...\n"
 
@@ -40,6 +40,6 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin main
+git push origin HEAD:main
 
 # git push origin HEAD:main alternatively
